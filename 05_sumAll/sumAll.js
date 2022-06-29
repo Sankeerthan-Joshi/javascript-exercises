60b33be142782cd194e042ff751226426c8efb95
Sankeerthan <sankeerthana.a.j@gmail.com>
2022-06-29 20:49:40 +0530

    Exercise 5 completed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    if((typeof(num1)!="number" || typeof(num1)!==typeof(num2) || Math.min(num1,num2)<0)){
+        return 'ERROR'
+    }
+    let sum =0;
+    for(let i = Math.max(num1,num2); i>= Math.min(num1,num2); i--){
+        sum +=i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
