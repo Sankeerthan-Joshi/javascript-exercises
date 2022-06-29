23b0b2616e4c65efaa37d9d289986e8d8430d448
Sankeerthan <sankeerthana.a.j@gmail.com>
2022-06-29 20:16:20 +0530

    Exercise 4 completed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,6 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) {
+    return newArray = array.filter((element)=>{return !args.includes(element)
+    })
 };
 
 // Do not edit below this line
