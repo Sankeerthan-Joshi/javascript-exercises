c8cac27ae58e6a6a9a6dec79ded536b3df6ba9df
Sankeerthan <sankeerthana.a.j@gmail.com>
2022-06-29 21:08:58 +0530

    Exercise 6 completed

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,14 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if(!(year%4)){
+        if(!(year%25)){
+            if(!(year%8)){
+                return true;
+            }
+            return false;
+        }
+        return true;
+    }
+    return false;
 };
 
 // Do not edit below this line
