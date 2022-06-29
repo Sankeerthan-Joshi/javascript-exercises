70dbe7e1e85a028353d5467583c5b25501ccf062
Sankeerthan <sankeerthana.a.j@gmail.com>
2022-06-29 20:00:32 +0530

    Exercise 3 completed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let array = str.split('');
+    str = '';
+    for(let i in array){
+        str += array[array.length -1-i];
+    }
+    return str;
 };
 
 // Do not edit below this line
