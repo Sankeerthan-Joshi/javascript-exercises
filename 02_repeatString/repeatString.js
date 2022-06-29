b254a26fa44d30ef16fe50b81628cd37d3c1f8e8
Sankeerthan <sankeerthana.a.j@gmail.com>
2022-06-29 19:48:49 +0530

    Exercise 1 & 2 completed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(word, repeatValue) {
+    if(repeatValue<0){
+        return 'ERROR'
+    }
+    if(repeatValue===0){
+        return "";
+    }
+    return word= word + repeatString(word,repeatValue-1);
 };
 
 // Do not edit below this line
