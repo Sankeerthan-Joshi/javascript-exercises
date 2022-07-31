c1481028a643b998082c0817db5f6f7571cb9236
Sankeerthan <sankeerthana.a.j@gmail.com>
2022-07-31 22:44:42 +0530

    Exercise 10 completed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if(num<0 || isNaN(parseInt(num))){
+        return 'OOPS';
+    }
+    num = parseInt(num);
+    if(num===1 || num===2){
+        return 1;
+    }
+    return fibonacci(num-1)+fibonacci(num-2)
 };
+
+console.log(fibonacci(4))
 
 // Do not edit below this line
 module.exports = fibonacci;
