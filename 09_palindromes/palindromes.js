6561b1b9bf86b20d706d503663332afd931d2cf9
Sankeerthan <sankeerthana.a.j@gmail.com>
2022-07-31 22:25:21 +0530

    Exercise 9 completed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let arr = str.toLowerCase().split("");
+    arr = arr.filter((element)=>(element>'a'&& element<='z'));
+    str = arr.join("");
+    for(let i=0; i<=str.length/2; i++){
+        if(str[i]!== str[str.length-1-i]){
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
